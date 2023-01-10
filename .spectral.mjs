c3f2106ea4d58664c1df568335bf11e9dcecefdb
carol.tfar@gmail.com <carol.tfar@gmail.com>
2023-01-10 17:21:55 -0500

    Adding product event sync api

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEyMTI2Ng/spectral.js?branch_slug=master&token=3f8c92c2-4800-4451-a014-e5415c24b8dc";
+export default { extends: ruleset };
